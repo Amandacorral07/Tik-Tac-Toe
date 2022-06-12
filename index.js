189d2ef0e135b8b589714970c6a2a166666dacf3
amandacorral07 <amandalewis0707@gmail.com>
2022-06-11 17:26:46 -0700

    Input reset button, player tracker, and winner function

--- index.js
+++ index.js
@@ -60,26 +60,32 @@ let playerTurn ="X"
 let notClicked = [true , true , true , true , true , true , true , true , true]
 let alreadyClicked = "This box is taken"
 
+let turnNum= 1
 
 function boxClick (blockNumber) {
     let currentBox = document.getElementById(`box${blockNumber}`)
-    // currentBox.addEventListener('click', function (event){
     if(notClicked[blockNumber-1] === true){
         if (playerTurn === "X" ){
           notClicked[blockNumber-1]= false; 
         currentBox.innerText = ('X')
         playerTurn = "O"
+        turnNum = turnNum +1 
+        document.getElementById('playerMove').innerText =('Player 2\'s turn')
         return;
     } else if (playerTurn === "O"){
         notClicked[blockNumber-1]= true;
         currentBox.innerText = ('O')
         playerTurn = "X" 
+        turnNum = turnNum +1 
+        document.getElementById('playerMove').innerText =('Player 1\'s turn')
         return;
     }
+}     
 }
-}
+
+
 // function to check for win
- function checkWin (){
+function checkWin (){
     if (box1.innerText != "" && box1.innerText === box2.innerText && box1.innerText === box3.innerText || 
         box4.innerText != "" && box4.innerText === box5.innerText && box4.innerText === box6.innerText ||
         box7.innerText != "" && box7.innerText === box8.innerText && box7.innerText === box9.innerText ||
@@ -89,12 +95,13 @@ function boxClick (blockNumber) {
         box1.innerText != "" && box1.innerText === box5.innerText && box1.innerText === box9.innerText || 
         box3.innerText != "" && box3.innerText === box5.innerText && box3.innerText === box7.innerText      
     ){
-        document.getElementById('winner').innerText= "We have a winner!"
-        console.log("We have a winner!")
-        
-        // endGame()
+        document.getElementById('winner').innerText= playerTracker()
     }
- }
+    if (turnNum === 9){
+        document.getElementById('winner').innerText= `It's a tie!` 
+    }
+    console.log("We have a winner!")
+}
 
 // calling each box 
 let box1 = document.getElementById('box1')
@@ -106,23 +113,36 @@ let box7 = document.getElementById('box7')
 let box8 = document.getElementById('box8')
 let box9 = document.getElementById('box9')
 
+// calling Player's move spot
+let playerMove= document.getElementById('playerMove')
+
+let winner= document.getElementById('winner')
+
 // reset button
 // let restartButton = document.getElementById('restartButton')
-function restartButton (blockNumber){
-    for (let i =1; i<=9; i++){
-        let currentBox= (`box${blockNumber}`)
-        notClicked[currentBox-1];
-        document.querySelector('box').remove('box'.innerText)
-        console.log("reset")
-    }
+function restartButton (){
+    box1.innerText =""
+    box2.innerText =""
+    box3.innerText =""
+    box4.innerText =""
+    box5.innerText =""
+    box6.innerText =""
+    box7.innerText =""
+    box8.innerText =""
+    box9.innerText =""
+    playerMove.innerText=""
+    winner.innerText =""
 }
 
-// endgame function 
-
-function endGame(tie){
-    if (tie) {
-        document.getElementById('player-turn').innerText= "It's a draw!"
+// // Player turn tracker function 
+function playerTracker(){
+    if (playerTurn === "X" && turnNum ===[5,7,9]){
+        playerMove.innerText=""
+        return "Player 2 has won!"
+        
     } else {
-        document.getElementById('player-turn').innerText= `Player with ${ "O's":"X's"} wins!`
+        playerMove.innerText=""
+        return "Player 1 has won!"
+        
     }
 }
